3313840d910d3a6ee00fd42accfb42662db0014f
Kidus Seleshi <kidusseleshi19@gmail.com>
2025-11-07 13:29:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8502982191-c4a75",
+  "appId": "1:971302053046:web:bd476a24493638e410f217",
+  "apiKey": "AIzaSyDmpnRWS0_Fl6E1elTGWjwMvic3RVh9wY4",
+  "authDomain": "studio-8502982191-c4a75.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "971302053046"
+};
