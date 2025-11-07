2058fb18ce64294e040df638abbb43132c3f3eea
Kidus Seleshi <kidusseleshi19@gmail.com>
2025-11-07 09:12:16 +0000

    Still not showing

--- next.config.ts
+++ next.config.ts
@@ -12,33 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 's3.tradingview.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.tradingview.com',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
