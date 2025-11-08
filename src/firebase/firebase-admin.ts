0d8c71b235493aace39215024119eca53e3a95f1
Kidus Seleshi <kidusseleshi19@gmail.com>
2025-11-08 10:18:23 +0000

    When I'm try to change my profile in setting avatar url it's say Error:

--- src/firebase/firebase-admin.ts
+++ src/firebase/firebase-admin.ts
@@ -1,18 +1,18 @@
 
 import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  : undefined;
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
 
 function initializeFirebaseAdmin(): App {
   if (getApps().length > 0) {
     return getApps()[0];
   }
+  
+  const credential = serviceAccount ? cert(JSON.parse(serviceAccount)) : undefined;
 
-  return initializeApp({
-    credential: cert(serviceAccount),
-  });
+  // When credential is not passed, SDK will automatically look for credentials
+  // from the environment. This is the correct behavior for App Hosting.
+  return initializeApp(credential ? { credential } : undefined);
 }
 
 export { initializeFirebaseAdmin };
