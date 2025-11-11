f5c8f84630e7567153f144a0bb7c0898d5e6b088
Kidus Seleshi <kidusseleshi19@gmail.com>
2025-11-11 08:48:59 +0000

    exactly did you have also dark theme

--- src/components/widgets/forex-heatmap-widget.tsx
+++ src/components/widgets/forex-heatmap-widget.tsx
@@ -1,12 +1,26 @@
 
 'use client';
 
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useTheme } from 'next-themes';
 
 export function ForexHeatmapWidget() {
+  const { theme } = useTheme();
+
+  const widgetSrc = useMemo(() => {
+    const config = {
+      width: '100%',
+      height: '100%',
+      currencies: ["EUR", "USD", "JPY", "GBP", "CHF", "AUD", "CAD", "NZD"],
+      colorTheme: theme === 'dark' ? 'dark' : 'light',
+      isTransparent: false,
+    };
+    return `https://www.tradingview-widget.com/embed-widget/forex-heat-map/?locale=en#${encodeURIComponent(JSON.stringify(config))}`;
+  }, [theme]);
+
   return (
     <iframe
-      src="https://www.tradingview-widget.com/embed-widget/forex-heat-map/?locale=en#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22currencies%22%3A%5B%22EUR%22%2C%22USD%22%2C%22JPY%22%2C%22GBP%22%2C%22CHF%22%2C%22AUD%22%2C%22CAD%22%2C%22NZD%22%5D%2C%22colorTheme%22%3A%22light%22%2C%22isTransparent%22%3Afalse%7D"
+      src={widgetSrc}
       style={{
         width: '100%',
         height: '100%',
