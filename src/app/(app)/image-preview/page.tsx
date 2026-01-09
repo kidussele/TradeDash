87f8178421adb0c53b1cfcc6084c33dfa4748cb7
kidusseleshi19 <kidusseleshi19@gmail.com>
2026-01-09 11:34:14 +0000

    page update

--- src/app/(app)/image-preview/page.tsx
+++ src/app/(app)/image-preview/page.tsx
@@ -3,6 +3,7 @@
 
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
+import Image from 'next/image';
 
 function ImagePreview() {
     const searchParams = useSearchParams();
@@ -14,9 +15,13 @@ function ImagePreview() {
 
     return (
         <div className="flex items-center justify-center h-screen bg-black p-4">
-            {/* Using <img> directly to avoid Next.js Image optimization issues with unknown external URLs */}
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={imageUrl} alt="Screenshot preview" className="max-w-full max-h-full object-contain animate-in fade-in-0 zoom-in-95 duration-500" />
+            <Image 
+                src={imageUrl} 
+                alt="Screenshot preview" 
+                layout="fill" 
+                objectFit="contain" 
+                className="animate-in fade-in-0 zoom-in-95 duration-500" 
+            />
         </div>
     );
 }
